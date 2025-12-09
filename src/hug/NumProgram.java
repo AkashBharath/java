da5405d09dc1032715ee81abbd460de5d57473cd
Akash bharath <akasbharath007@gmail.com>
2025-12-09 13:20:13 +0530

    Initial commit

---
+++ src/hug/NumProgram.java
@@ -0,0 +1,21 @@
+package hug;
+
+public class NumProgram {
+	
+	    public static void main(String[] args) {
+	        int num = 153, temp = num, sum = 0;
+
+	        while (temp > 0) {
+	            int digit = temp % 10;
+	            sum += digit * digit * digit;
+	            temp /= 10;
+	        }
+
+	        if (num == sum)
+	            System.out.println("Armstrong number");
+	        else
+	            System.out.println("Not an Armstrong number");
+	    }
+	}
+
+
